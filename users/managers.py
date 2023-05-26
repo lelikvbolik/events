7c28824e8896ccee22501f1884a8e8ce9aa10fb3
alex <alex.lya@mail.ru>
2023-05-26 14:24:15 +0300

    first commit

---
+++ users/managers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("Users must have an email address"))
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password):
+        """Метод создаёт и возвращает модель `User` с правами
+        суперпользователя (админа)."""
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
